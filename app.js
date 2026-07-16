4d467af9e7eb8f502af8ffa288d3af415c3121ba
Lujav Gonzalez <lujavgonzalez@gmail.com>
2026-07-15 22:39:13 -0600

    Se agregan modelos

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ for (let i = 0; i < len; i++) {
   text += cars[i] + "<br>";
 }
 
+const model = ["Mazda 3", "Seat Ibiza", "BMW", "Ford"];
+let len2 = model.length;
+
+let text2 = "";
+for (let i = 0; i < len2; i++) {
+  text2 += model[i] + "<br>";
+}
+
 document.getElementById("demo").innerHTML = text;
+document.getElementById("demo2").innerHTML = text2;
